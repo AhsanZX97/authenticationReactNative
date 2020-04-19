33a2df9f49465adc7bd72928c32f194576fe68ea
Ahsan Zia <ahsan97@hotmail.co.uk>
2020-04-19 12:33:20 +0100

    made login page

--- components/emailAndPassword.js
+++ components/emailAndPassword.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View, ClippingRectangle } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 class EmailAndPassword extends React.Component {
+
+    state = {
+        email: '',
+        password: '',
+        error: ''
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text>Open up emailAndPassword.js to start working on your emailAndPassword!</Text>
+                <TextInput placeholder="email" style={styles.input}
+                    value={this.state.email}
+                    onChangeText={email => this.setState({ email })} />
+
+                <TextInput placeholder="password" style={styles.input}
+                    value={this.state.password}
+                    onChangeText={password => this.setState({ password })} />
+
+                <TouchableOpacity style={styles.buttonContainer}>
+                    <Text style={styles.buttonText}>Login</Text>
+
+                </TouchableOpacity>
+                <Text style={styles.errorText}>
+                    {this.state.error}
+                </Text>
             </View>
         )
     }
@@ -14,8 +35,35 @@ class EmailAndPassword extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center'
+        padding: 20
     },
+    input: {
+        height: 40,
+        backgroundColor: 'rgba(255,255,255,.5)',
+        paddingLeft: 10,
+        marginBottom: 15,
+        borderRadius: 5,
+        fontSize: 15
+    },
+    errorText: {
+        fontSize: 15,
+        color: 'red',
+        alignSelf: 'center',
+        marginTop:15
+    },
+
+    buttonText: {
+        textAlign: 'center',
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 20
+    },
+
+    buttonContainer: {
+        backgroundColor: '#3B3B98',
+        padding: 15,
+        borderRadius: 8
+    }
 });
 
 export default EmailAndPassword;
