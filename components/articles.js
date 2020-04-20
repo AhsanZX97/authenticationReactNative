a3ba583086dcfbd5af151501057c9f69ee9c1f52
Ahsan Zia <ahsan97@hotmail.co.uk>
2020-04-20 11:34:23 +0100

    article page setup

--- components/articles.js
+++ components/articles.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View, ClippingRectangle } from 'react-native';
+import { StyleSheet, Text, View, ClippingRectangle, TouchableOpacity } from 'react-native';
 
 class Articles extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>Open up articles.js to start working on your articles!</Text>
+                <View style={styles.articleContainer}>
+                    <Text style={styles.header}>
+                        Welcome to the authorised screen
+                    </Text>
+                    <Text style={styles.content}>
+                        You are logged in from firebase
+                    </Text>
+                    <TouchableOpacity style={{padding:20}}>
+                        <Text style={{color:'#1B9CFC'}}>LogOut</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         )
     }
@@ -18,6 +28,21 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    articleContainer: { 
+        padding:10,
+        borderBottomColor:'rgba(255,255,255,.7)',
+        borderBottomWidth:5
+    },
+    header:{
+        fontSize:22,
+        color:'black',
+        padding:10,
+        marginBottom:5
+    },
+    content: {
+        marginTop:10,
+        fontSize:19,
+    }
 });
 
 export default Articles;
